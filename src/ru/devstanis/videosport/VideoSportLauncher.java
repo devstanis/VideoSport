82e4ae76c879a27f66c65fdc02b9db368a38351f
devstanis(PC) <devstanis@gmail.com>
2018-10-07 20:17:11 +0300

    Создал пакет и стартовый класс

---
+++ src/ru/devstanis/videosport/VideoSportLauncher.java
@@ -0,0 +1,9 @@
+package ru.devstanis.videosport;
+
+public class VideoSportLauncher
+{
+    public static void main(String[] args)
+    {
+        System.out.println("VideoSport is coming!");
+    }
+}
